e3a21a08b14d26d2538ada966b0afdc882611b29
KraftaN <63770425+MalteBGM@users.noreply.github.com>
2021-07-04 23:16:19 +0200

    Update level.js

--- commands/special/level.js
+++ commands/special/level.js
@@ -1,5 +1,6 @@
 const Levels = require("discord-xp");
 const mongo = require("../../utility/mongo.js");
+const functions = require("../../utility/functions.js");
 
 module.exports = {
 	name: "level",
@@ -9,7 +10,7 @@ module.exports = {
 	manArgs: 1,
 	cooldown: 1,
 	usage: "<@someone>",
-	async execute (message, args) {
+	async execute(message, args) {
 		await mongo().then(async (mongoose) => {
 			try {
 				const target = message.mentions.users.first() || message.author; // Grab the target.
@@ -19,7 +20,11 @@ module.exports = {
 				if (!user)
 					return message.channel.send("Seems like this user has not earned any xp so far."); // If there isnt such user in the database, we send a message in general.
 
-				message.channel.send(`> **${target.tag}** is currently level **${user.level}** in **${message.guild.name}**`); // We show the level.
+				message.channel.send(
+					embedify(
+						`> **${target.tag}** is currently level **${user.level}** in **${message.guild.name}**`
+					)
+				); // We show the level.
 			} finally {
 				mongoose.connection.close();
 			}
