15394acf15412835f404f491a17faede39d98653
KraftaN <63770425+MalteBGM@users.noreply.github.com>
2021-07-05 09:25:37 +0200

    Update test.js

--- commands/misc/test.js
+++ commands/misc/test.js
@@ -13,8 +13,41 @@ module.exports = {
 	cooldown: 0,
 	args: false,
 	execute(message, args, guild) {
-		bot.users.fetch(message.author.id).then((dm) => {
-			dm.send("Message to send");
+		const configQuestions = ["Question 2", "Question 3", "Question 4"];
+		let config = [];
+
+		let i = 0;
+		let userData;
+
+		message.author.send("Question 1").then((m) => {
+			const collector = m.channel.createMessageCollector(
+				(me) => me.author.id === message.author.id && me.channel === m.channel,
+				{
+					time: 120 * 1000, // This could be any amount of time
+				}
+			);
+			collector.on("collect", (collected) => {
+				config.push(collected.content);
+				if (i >= configQuestions.length) {
+					collected.channel.send("Stopped collecting");
+					collector.stop();
+					return;
+				}
+				collected.channel.send(configQuestions[i]);
+
+				userData = collected.channel;
+
+				i += 1;
+			});
+			collector.on("end", (collected, reason) => {
+				if (reason === "time")
+					// This checks if the time ran out
+					return message.author.send(
+						`You ran out of time. Write \`.command\` to reinitiate this command. `
+					);
+				console.log(config);
+				return config;
+			});
 		});
 	},
 };
