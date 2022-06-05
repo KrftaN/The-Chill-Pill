cc8828513ade41bcb95b13b3a0b64ce509cec28c
KraftaN <63770425+MalteBGM@users.noreply.github.com>
2022-06-06 01:38:57 +0200

    github fuck-up

---
+++ utility/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	opt: {
+		maxVol: 200,
+		loopMessage: false, //Please don't touch otherwise I will kill you
+		discordPlayer: {
+			ytdlOptions: {
+				quality: "highestaudio", //Please don't touch or I'll touch your baby
+				highWaterMark: 1 << 25, //Please don't touch otherwise you'll die
+				filter: "audioonly",
+			},
+		},
+	},
+
+};
